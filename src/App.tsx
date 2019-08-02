a1876766ab058f9772cebc3bcb3e36165578116f
lyoz <lyoz@users.noreply.github.com>
2019-08-02 11:19:19 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import Characters from './components/Characters';
-import Home from './components/Home';
 import './App.css';
 
-const App: React.FC = () => (
-  <div className="container">
-    <Switch>
-      <Route path="/characters/:code" component={Characters} />
-      <Route path="/" component={Home} />
-      <Redirect to="/" />
-    </Switch>
-  </div>
-);
+const App: React.FC = () => <div className="container">Hello, world.</div>;
 
 export default App;
