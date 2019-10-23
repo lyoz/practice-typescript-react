3b9a13c41e3c1d17dd3ba44ad4bf5289c19cf90f
lyoz <lyoz@users.noreply.github.com>
2019-10-24 07:47:13 +0900

    simplify

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Hello from './components/Hello'
+
+const App: React.FC = () => (
+  <Hello compiler='TypeScript' framework='React' />
+)
+
+export default App
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react'
+import React from 'react'
 
-export interface HelloProps {
+interface HelloProps {
   compiler: string
   framework: string
 }
 
-class Hello extends React.Component<HelloProps, {}> {
-  render() {
-    const { compiler, framework } = this.props
-    return <h1> Hello from {compiler} and {framework}</h1>
-  }
+const Hello: React.FC<HelloProps> = (props) => {
+  const { compiler, framework } = props
+  return <h1> Hello from {compiler} and {framework}</h1>
 }
 
 export default Hello
