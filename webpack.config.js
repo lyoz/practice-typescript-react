41b765325602e076d023b3fc2eb864c7ddb0be10
lyoz <1482802+lyoz@users.noreply.github.com>
2024-04-22 10:42:59 +0900

    format

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new ForkTsCheckerWebpackPlugin(),
-  ],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  devServer: {
-    static: "dist",
-    historyApiFallback: true,
-  },
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "src/index.html",
+		}),
+		new ForkTsCheckerWebpackPlugin(),
+	],
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	devServer: {
+		static: "dist",
+		historyApiFallback: true,
+	},
 };
